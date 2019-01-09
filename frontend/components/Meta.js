73f631eca73283a815ba68ec7810d952bf495440
David G Chaves <davidgchaves@gmail.com>
2019-01-09 11:26:45 +0100

    Fix `charSet` and `httpEquiv` in <Meta>

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -3,9 +3,9 @@ import Head from "next/head";
 
 const Meta = _props => (
   <Head>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
     <link rel="shortcut icon" href="static/favicon.png" type="image/x-icon" />
     <link rel="stylesheet" href="static/nprogress.css" type="text/css" />
     <title>Document</title>
